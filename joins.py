6f4f55fadcfb6a8eff2b6d076eb819ce151c02bf
Jegadesh S <159524134+jegadesh-github@users.noreply.github.com>
2025-05-28 13:37:21 +0530

    Update joins.py

--- joins.py
+++ joins.py
@@ -4,7 +4,7 @@ import mysql.connector
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'Password123!',
+    'password': 'yourpassword!',
     'database': 'spotify_db'
 }
 
