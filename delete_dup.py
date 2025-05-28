a2e5f9d79b8f62afa9ea224936e83d5d19ff6960
Jegadesh S <159524134+jegadesh-github@users.noreply.github.com>
2025-05-28 13:32:24 +0530

    Update delete_dup.py

--- delete_dup.py
+++ delete_dup.py
@@ -5,8 +5,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import _mysql_connector
 
 sp=spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id='6f9dcb59b70c46a588d5f2b300d639d6',
-    client_secret='fde88152da554ed38b662ecc1f8689dc'
+    client_id='your client id',
+    client_secret='your secret id'
 ))
 db_config = {
     'host': 'localhost',
