8f818c85b1a75acba6e26cdbd7da1a08ecedfb37
Jegadesh S <159524134+jegadesh-github@users.noreply.github.com>
2025-05-28 13:27:59 +0530

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -9,8 +9,8 @@ import re
 
 
 sp=spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id='6f9dcb59b70c46a588d5f2b300d639d6',
-    client_secret='fde88152da554ed38b662ecc1f8689dc'
+    client_id='your client id',
+    client_secret='your secret id'
 ))
 
 track_url="https://open.spotify.com/track/6SZgnc7BvRmSXErk0hyXkq?si=ebc1144f7b344cfd"
